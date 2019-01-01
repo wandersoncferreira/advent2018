29a3a57a8267361443ffc03f4045bafeab5d13fa
Wanderson Ferreira <iagwanderson@gmail.com>
2019-01-01 19:42:02 -0200

    solution for day4 part 1.
    I need to review one of the functions. The `guard-info`
    function is not easy to understand. Text parsing is always
    bad to reason about. But I believe that a better work can be
    done.

---
+++ src/advent2018/day4.clj
@@ -0,0 +1,79 @@
+(ns advent2018.day4
+  (:require [clojure.string :as s]
+            [clj-time.format :as f]
+            [clj-time.core :as t]))
+
+(def input (slurp "inputs/day4"))
+
+(defn guard-ids
+  [input]
+  (->> input
+       (re-seq #"Guard\s#(\d+)")
+       (map second)
+       distinct))
+
+(defn sort-input
+  [input-line]
+  (let [mtc (partial re-matcher #"\[(?<date>\d{4}-\d{2}-\d{2}\s\d{2}:\d{2})\]\s(?<msg>.*)")
+        fmt (f/formatter "yyyy-MM-dd HH:mm")
+        matcher (mtc input-line)]
+    (if (.matches matcher)
+      (hash-map (f/parse fmt (.group matcher "date")) (.group matcher "msg")))))
+
+(defn guard-info
+  [input guard-id]
+  (let [entries (s/split input #"\n")
+        data-sorted (->> entries
+                         (map sort-input)
+                         (into {})
+                         sort)]
+    (loop [entry data-sorted
+           current-gId guard-id
+           res []]
+      (if (nil? (first entry))
+        res
+        (if-let [current-gId (second (re-find #"Guard\s#(\d+)" (second (first entry))))]
+          (recur (rest entry) current-gId res)
+          (if (= current-gId guard-id)
+            (recur (rest entry) current-gId (cons [(t/day (first (first entry)))
+                                                   (t/minute (first (first entry)))] res))
+            (recur (rest entry) current-gId res)))))))
+
+(defn guard-total-sleep
+  [guard-info]
+  (->> guard-info
+       (map second)
+       (partition-all 2)
+       (map #(- (first %) (second %)))
+       (reduce +)))
+
+(defn guard-min-most-slept
+  [guard-info]
+  (->> guard-info
+     (map second)
+     (partition-all 2)
+     (map #(range (second %) (first %)))
+     flatten
+     frequencies
+     (sort-by val >)
+     ((comp first first))))
+
+(defn index-max-val
+  [coll]
+  (->> coll
+       (map-indexed vector)
+       (apply max-key second)
+       first))
+
+(defn solve-day4
+  [input]
+  (let [ids (guard-ids input)
+        infos (map (partial guard-info input) ids)
+        total-sleeps (map guard-total-sleep infos)
+        max-slept-id (index-max-val total-sleeps)]
+    (* (guard-min-most-slept (nth infos max-slept-id))
+       (read-string (nth ids max-slept-id)))))
+
+(solve-day4 input)
+;; => 101262
+
